969a3346817070f2b5c30c705ffe470183c8dede
cocktailphp <cocktailo115@gmail.com>
2017-02-13 04:36:11 +0100

    Add apply method to workflow trait

--- src/Traits/WorkflowTrait.php
+++ src/Traits/WorkflowTrait.php
@@ -6,6 +6,11 @@ use Workflow;
 
 trait WorkflowTrait
 {
+    public function workflow_apply($transition, $workflow = null)
+    {
+        return Workflow::get($this, $workflow)->apply($this, $transition);
+    }
+
     public function workflow_can($transition, $workflow = null)
     {
         return Workflow::get($this, $workflow)->can($this, $transition);
