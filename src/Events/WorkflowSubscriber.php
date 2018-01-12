143047056755e57a75782d24f470e968e7b4c624
cocktailphp <cocktailo115@gmail.com>
2018-01-12 15:20:01 +0100

    Added more event subscribers to conform to the way Symfony has the events set up. Also updated and added assert statements.

--- src/Events/WorkflowSubscriber.php
+++ src/Events/WorkflowSubscriber.php
@@ -11,39 +11,54 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  */
 class WorkflowSubscriber implements EventSubscriberInterface
 {
-    public function guardEvent(SymfonyGuardEvent $event) {
+    public function guardEvent(SymfonyGuardEvent $event)
+    {
         event(new GuardEvent($event));
-        event('workflow.'.$event->getWorkflowName().'.guard', $event);
+        event('workflow.guard', $event);
+        event('workflow.' . $event->getWorkflowName() . '.guard', $event);
+        event('workflow.' . $event->getWorkflowName() . '.guard.' . $event->getTransition()->getName(), $event);
     }
 
-    public function leaveEvent(Event $event) {
+    public function leaveEvent(Event $event)
+    {
         event(new LeaveEvent($event));
-        event('workflow.'.$event->getWorkflowName().'.leave', $event);
+        event('workflow.leave', $event);
+        event('workflow.' . $event->getWorkflowName() . '.leave', $event);
+        event('workflow.' . $event->getWorkflowName() . '.leave.' . $event->getTransition()->getName(), $event);
     }
 
-    public function transitionEvent(Event $event) {
+    public function transitionEvent(Event $event)
+    {
         event(new TransitionEvent($event));
-        event('workflow.'.$event->getWorkflowName().'.transition', $event);
+        event('workflow.transition', $event);
+        event('workflow.' . $event->getWorkflowName() . '.transition', $event);
+        event('workflow.' . $event->getWorkflowName() . '.transition.' . $event->getTransition()->getName(), $event);
     }
 
-    public function enterEvent(Event $event) {
+    public function enterEvent(Event $event)
+    {
         event(new EnterEvent($event));
-        event('workflow.'.$event->getWorkflowName().'.enter', $event);
+        event('workflow.enter', $event);
+        event('workflow.' . $event->getWorkflowName() . '.enter', $event);
+        event('workflow.' . $event->getWorkflowName() . '.enter.' . $event->getTransition()->getName(), $event);
     }
 
-    public function enteredEvent(Event $event) {
+    public function enteredEvent(Event $event)
+    {
         event(new EnteredEvent($event));
-        event('workflow.'.$event->getWorkflowName().'.entered', $event);
+        event('workflow.entered', $event);
+        event('workflow.' . $event->getWorkflowName() . '.entered', $event);
+        event('workflow.' . $event->getWorkflowName() . '.entered.' . $event->getTransition()->getName(), $event);
     }
 
     public static function getSubscribedEvents()
     {
         return [
-            'workflow.guard'        => ['guardEvent'],
-            'workflow.leave'        => ['leaveEvent'],
-            'workflow.transition'   => ['transitionEvent'],
-            'workflow.enter'        => ['enterEvent'],
-            'workflow.entered'      => ['enteredEvent'],
+            'workflow.guard'      => ['guardEvent'],
+            'workflow.leave'      => ['leaveEvent'],
+            'workflow.transition' => ['transitionEvent'],
+            'workflow.enter'      => ['enterEvent'],
+            'workflow.entered'    => ['enteredEvent'],
         ];
     }
 }
